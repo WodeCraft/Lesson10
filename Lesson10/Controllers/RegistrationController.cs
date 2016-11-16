2fb20801afa2f3960057650221a54f7e50f3fe85
Jens Christian Rasch <jcr@wodecraft.dk>
2016-11-16 10:15:09 +0100

    Initial commit

---
+++ Lesson10/Controllers/RegistrationController.cs
@@ -0,0 +1,29 @@
+﻿using Lesson10.Models;
+using System.Web.Mvc;
+
+namespace Lesson10.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateBooking()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateBooking(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            return View("Completed");
+        }
+    }
+}
---
+++ Lesson10/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lesson10.Models
+{
+    public class User
+    {
+        [Required]
+        [MinLength(4)]
+        public string Fullname { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Range(1, 120)]
+        public int Age { get; set; }
+
+
+    }
+}
